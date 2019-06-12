31fd852aea710001a55a7b79ca13cea9be801bd8
Divya Singh <divsin@microsoft.com>
2019-06-12 14:47:11 +0530

    chk for changes

--- HelloWebApi/HelloApiController.cs
+++ HelloWebApi/HelloApiController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Dynamic;
 using System.Web.Http;
 
 namespace HelloWebApi
@@ -9,6 +10,7 @@ namespace HelloWebApi
     public class HelloApiController : ApiController
     {
         public string Get() {
+            Console.WriteLine("Conflict check");
             return "hello";
         }
     }
