74f041072b5da110acb338981b8d194bb0d9a257
Divya Singh <divsin@microsoft.com>
2019-06-12 14:44:25 +0530

    change for chk

--- HelloWebApi/HelloApiController.cs
+++ HelloWebApi/HelloApiController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Configuration;
 using System.Web.Http;
 
 namespace HelloWebApi
@@ -9,7 +10,9 @@ namespace HelloWebApi
     public class HelloApiController : ApiController
     {
         public string Get() {
+            Console.WriteLine("Conflict");
             return "hello";
+
         }
     }
 }
