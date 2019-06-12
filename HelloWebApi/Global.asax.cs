8935a007f5c5c4dc3642f7cba17fa1206461b19e
Divya Singh <divsin@microsoft.com>
2019-06-12 09:56:15 +0530

    first commit

---
+++ HelloWebApi/Global.asax.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Routing;
+using System.Web.Http.Routing;
+
+namespace HelloWebApi
+{
+    public class WebApiApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            GlobalConfiguration.Configuration.Routes.Add("default", new HttpRoute("{controller}"));
+        }
+    }
+}
