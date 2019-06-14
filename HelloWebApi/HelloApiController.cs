76e40ba663aaafa4e70c11aadfef27b7b39420bb
Divya Singh <divsin@microsoft.com>
2019-06-14 14:39:45 +0530

    new branch2  commit 1

--- HelloWebApi/HelloApiController.cs
+++ HelloWebApi/HelloApiController.cs
@@ -2,14 +2,18 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Configuration;
 using System.Web.Http;
 
 namespace HelloWebApi
 {
     public class HelloApiController : ApiController
     {
-        public string Get() {
-            return "hello yo ho";
+        public string Get()
+        {
+            Console.WriteLine("Conflict it is");
+            return "hello";
+
         }
     }
 }
